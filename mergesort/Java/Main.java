33fdde97916149bc0f16ee7e58847c06bb0be14f
MuhiaKevin <archerkreiger@gmail.com>
2019-04-22 13:57:49 +0300

    added merge function for java

--- mergesort/Java/Main.java
+++ mergesort/Java/Main.java
@@ -1,6 +1,9 @@
+/*
+    https://en.wikipedia.org/wiki/Merge_sort
+    https://www.geeksforgeeks.org/merge-sort/
+    https://springframework.guru/merge-sort-in-java/
 
-// https://en.wikipedia.org/wiki/Merge_sort
-// https://www.geeksforgeeks.org/merge-sort/
+*/
 
 import java.util.Arrays;
 
@@ -9,7 +12,7 @@ class MergeSort{
     int[] mergeSort(int [] array){
 
         // if array has one element then it is sorted
-        if(array.length < 2){
+        if(array.length <= 1){
             return array;
         }
 
@@ -17,24 +20,44 @@ class MergeSort{
         int mid = arraylen / 2;
         int [] leftArray = Arrays.copyOfRange(array, 0, mid);
         int [] rightArray = Arrays.copyOfRange(array, mid, arraylen);
-        System.out.println(Arrays.toString(rightArray));
+        // System.out.println(Arrays.toString(rightArray));
 
+         // recursively call mergesort to divide the array until there is only one element left in
         mergeSort(leftArray);
         mergeSort(rightArray);
-
+        
+        merge(leftArray,rightArray);
 
         return array;
     }
-
+    
+    void merge(int [] leftArray, int [] rightArray){
+        int indexofleftarray = 0;
+        int indexofrightarray = 0;
+        int indexOfMergedList = 0;
+        int [] sortedArray = {};
+        
+        while(indexofleftarray < leftArray.length && indexofrightarray < rightArray.length){
+            
+            if(leftArray[indexofleftarray] < rightArray[indexofrightarray]){
+                sortedArray[indexOfMergedList] = leftArray[indexofleftarray];
+                indexofleftarray++;
+            }
+            else{
+                sortedArray[indexOfMergedList] = rightArray[indexofrightarray];
+                indexofrightarray++;
+            }
+            
+        }
+    
+        System.out.println(Arrays.toString(sortedArray));    
+    }
 
 }
 
 
 
-
-
 public class Main {
-
     public static void main(String[] args) {
         int [] array = {12,45,6,5,6,8};
         MergeSort mrgsort = new MergeSort();
